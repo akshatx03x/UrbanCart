bcbd50f024fc1b1925d062d63df7e87802c7dc68
Akshat Gupta <akshatx03x@gmail.com>
2025-08-29 19:50:28 +0530

    Day2

--- src/components/LatestCollection.jsx
+++ src/components/LatestCollection.jsx
@@ -24,7 +24,7 @@ const LatestCollection = () => {
       <div className="grid grid-cols-2 sm:grid-cols-3 md:grids-col-4 lg:grid-cols-5 gap-4 gap-y-6 ">
         {
             latestProducts.map((items, index)=>{
-                <ProductItem key={index} id={item_.id} />
+                <ProductItem key={index} />
             })
         }
       </div>
